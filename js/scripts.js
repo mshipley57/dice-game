e8686a1aa118983bbd997d86873f7734af51f11c
Bryce Frazier and Mariah Shipley <mariah@>
2018-01-24 14:31:01 -0800

    update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,46 @@
 //back-end logic
 
-function player(name, score) {
+function Player(name, score, running, total) {
   this.name = name;
-  this.score = score;
+  this.score = score; // total from previous round
+  this.running = running; //total from current round
+  this.total = total; //combined total
+}
+
+Player.prototype.playerScore = function() {
+  return this.name + ": " + this.total;
+}
+
+Player.prototype.addScore = function(points) {
+  this.total += points
+}
+
+Player.prototype.rolledOnes = function() {
+  this.score = 0
 }
 
 //function userScore = function(dice) {
 function randomNumber() {
   var number = Math.ceil(Math.random() * Math.ceil(6));
   if (number === 1) {
-    alert("Next player up!")
-  }  else {
-    (number === 2 || 3 || 4 || 5 || 6)
-    alert("Keep rolling!")
+    console.log ("Back to zero! Next player up!");
+  } else {
+    console.log("Keep rolling! You scored " + number + " this roll.");
   };
 };
   // if (diceArray(i) === 0) {
   //   alert("Next player up!")
   // };
-  randomNumber();
-  console.log(randomNumber);
+  // randomNumber();
 
 //  //front-end logic
-// $(document).ready(function(){
-//   $("#roll").click(function(event){
-//     event.preventDefault();
-//     var playerScore = randomNumber(min, max);
-//
-//
-//
-// });
-// });
+$(document).ready(function(){
+  $("#roll").click(function(event){
+    event.preventDefault();
+    var playerScore = randomNumber(min, max);
+    var inputtedName = $(this).submit("input#new-name").val();
+    var newPlayer = new Player (inputtedName);
+
+
+  });
+});
