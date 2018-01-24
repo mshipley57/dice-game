57743c59a24f93c304eb5b209e74898ca66f16e6
Bryce Frazier and Mariah Shipley <mariah@>
2018-01-24 11:46:36 -0800

    add html, js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,33 @@
+//back-end logic
 
+function player(name, score) {
+  this.name = name;
+  this.score = score;
+}
+
+//function userScore = function(dice) {
+function randomNumber() {
+  var number = Math.ceil(Math.random() * Math.ceil(6));
+  if (number === 1) {
+    alert("Next player up!")
+  }  else {
+    (number === 2 || 3 || 4 || 5 || 6)
+    alert("Keep rolling!")
+  };
+};
+  // if (diceArray(i) === 0) {
+  //   alert("Next player up!")
+  // };
+  randomNumber();
+  console.log(randomNumber);
+
+//  //front-end logic
+// $(document).ready(function(){
+//   $("#roll").click(function(event){
+//     event.preventDefault();
+//     var playerScore = randomNumber(min, max);
+//
+//
+//
+// });
+// });
